cbe1b67e880d67ff8448e17f38a7377abaa32aeb
lamierosa <uliaarseneva58@gmail.com>
2025-11-22 11:14:45 +0300

    process

---
+++ core/src/ru/samsung/gamestudio/character/Bird.java
@@ -0,0 +1,77 @@
+
+package ru.samsung.gamestudio.character;
+
+import static ru.samsung.gamestudio.MyGdxGame.SCR_HEIGHT;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public class Bird {
+
+    int x, y;
+    int width, height;
+
+    int speed;
+    int jumpHeight;
+    final int maxHeightOfJump = 100;
+    boolean jump = true;
+
+    int frameCounter;
+    Texture[] framesArray;
+
+    public Bird(int x, int y, int speed, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = width;
+        this.height = height;
+        frameCounter = 0;
+
+        framesArray = new Texture[]{
+                new Texture("pictures_for_game/bird/bird0.png"),
+                new Texture("pictures_for_game/bird/bird1.png"),
+                new Texture("pictures_for_game/bird/bird2.png"),
+                new Texture("pictures_for_game/bird/bird1.png"),
+        };
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void onClick() {
+        jump = true;
+        jumpHeight = maxHeightOfJump + y;
+    }
+
+    public void fly() {
+        if (y >= jumpHeight) {
+            jump = false;
+        }
+
+        if (jump) {
+            y += speed;
+        } else {
+            y -= speed/1.2;
+        }
+    }
+
+    public boolean isInField() {
+        if (y + height < 0) return false;
+        if (y > SCR_HEIGHT) return false;
+        return true;
+    }
+
+    public void draw(Batch batch) {
+        int frameMultiplier = 10;
+        batch.draw(framesArray[frameCounter / frameMultiplier], x, y, width, height);
+        if (frameCounter++ == framesArray.length * frameMultiplier - 1) frameCounter = 0;
+    }
+
+    public void dispose() {
+        for (Texture texture : framesArray) {
+            texture.dispose();
+        }
+    }
+
+}
---
+++ core/src/ru/samsung/gamestudio/screen/ScreenMenu.java
@@ -0,0 +1,87 @@
+package ru.samsung.gamestudio.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import ru.samsung.gamestudio.MyGdxGame;
+import ru.samsung.gamestudio.component.MovingBackground;
+import ru.samsung.gamestudio.component.TextButton;
+
+public class ScreenMenu implements Screen {
+    MyGdxGame myGdxGame;
+    MovingBackground background;
+    TextButton buttonStart;
+    TextButton buttonExit;
+
+    public ScreenMenu(MyGdxGame myGdxGame) {
+        this.myGdxGame = myGdxGame;
+
+        buttonStart = new TextButton(400,450,"Start");
+        buttonExit = new TextButton(400,150,"Exit");
+        background = new MovingBackground("pictures_for_game/background/restart_bg.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        if (Gdx.input.justTouched()) {
+
+            Vector3 touch = myGdxGame.camera.unproject(
+                    new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0)
+            );
+
+            if (buttonStart.isHit((int) touch.x, (int) touch.y)) {
+                myGdxGame.setScreen(myGdxGame.screenGame);
+            }
+
+            else if (buttonExit.isHit((int) touch.x, (int) touch.y)) {
+                Gdx.app.exit();
+            }
+
+        }
+
+        ScreenUtils.clear(1, 0, 0, 1);
+        myGdxGame.camera.update();
+        myGdxGame.batch.begin();
+
+        background.draw(myGdxGame.batch);
+        buttonStart.draw(myGdxGame.batch);
+        buttonExit.draw((myGdxGame.batch));
+
+        myGdxGame.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        buttonStart.dispose();
+        buttonExit.dispose();
+    }
+}
